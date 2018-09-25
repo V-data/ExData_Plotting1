cb1e6b9d7ac3f8c2edf020e69edd2813ebbcee4e
V-data <22335226+V-data@users.noreply.github.com>
2018-09-25 13:12:32 -0600

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,6 @@
+
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
