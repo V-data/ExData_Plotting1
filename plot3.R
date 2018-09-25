048b23bfa08bfe3d38cd15e91369b38006c6b886
V-data <22335226+V-data@users.noreply.github.com>
2018-09-25 13:48:53 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,32 @@
+# Load data 
+
+dataFile <- "./household_power_consumption.txt"
+power <- read.table(dataFile, header = TRUE, sep = ";", stringsAsFactors = FALSE, na.strings = "?")
+
+# Subset data
+
+sub_power <- power[power$Date %in% c("1/2/2007", "2/2/2007") ,]
+
+# Convert variable
+
+sub_power$Date <- as.Date(sub_power$Date, format = "%d/%m/%Y")
+datetime <- paste(as.Date(sub_power$Date), sub_power$Time)
+sub_power$Datetime <- as.POSIXct(datetime)
 
 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+# Plot
+
+with(sub_power, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+## Saving to file
+
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
